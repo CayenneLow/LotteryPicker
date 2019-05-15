1d2cdf42103265369c2914961d6f96ab53ad246e
Khye Ean <lowkhyeean@gmail.com>
2019-05-15 17:20:18 +1000

    first draft

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, redirect, render_template, request, session, url_for
+from flask_bootstrap import Bootstrap
+import random
+from rng import RNG, Error, Exhausted, checks
+app = Flask(__name__)
+Bootstrap(app)
+app.secret_key='very-secret-123'
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    session.clear()
+    if request.method == 'POST':
+        # calculate RNG
+        min = request.form['min']
+        max = request.form['max']
+        nNums = request.form['nNums']
+        session['min'] = min
+        session['max'] = max
+        session['nNums'] = nNums
+        try:
+            checks(int(min), int(max), int(nNums))
+        except Error as e:
+            return render_template('index.html', error=e)
+        return redirect(url_for('display'))
+    return render_template('index.html', error=None)
+
+@app.route('/display/', methods=['GET', 'POST'])
+def display():
+    if request.method == 'POST':
+        if request.form['button'] == 'Again?':
+            #refresh
+            return redirect(url_for('display'))
+        else:
+            return redirect(url_for('index'))
+    # ensure control flow
+    if 'min' not in session:
+        return redirect(url_for('index'))
+
+    min = int(session.get('min', None))
+    max = int(session.get('max', None))
+    nNums = int(session.get('nNums', None))
+    if 'seen' not in session:
+        print("yes")
+        session['seen'] = []     
+    seenList = session.get('seen', None)
+    exhausted = False
+    try:
+        session['seen'] = RNG(min, max, nNums, seenList)
+    except Exhausted as e:
+        exhausted = True
+    return render_template('display.html', printList=seenList, exhausted=exhausted)
+
+    
+
+if __name__ == '__main__':
+    app.run(debug=True, port=4000)
--- rng.py
+++ rng.py
@@ -4,51 +4,35 @@ class Error(Exception):
     def __init__(self, error):
         super().__init__(error)
 
-# global seen list for checking duplicates
-seen = []
-def RNG(min, max, nNum, iteration):
+class Exhausted(Exception):
+    def __init__(self, error):
+        super().__init__(error) 
+
+def RNG(min, max, nNum, seen):
+    checks(min,max,nNum)
+    # main logic
+    for x in range(nNum):
+        # no other numbers to pick from
+        if len(seen) >= max-min+1:
+            raise Exhausted("All numbers Exhausted")
+            break
+        returnList = seen
+        # keep looping if it's a duplicate number
+        while (1):
+            num = random.randint(min, max)
+            if num in returnList:
+                exist = True
+            else:
+                exist = False
+                # display num then add it to seen list
+                returnList.append(num)
+                break
+    return returnList
+
+def checks(min, max, nNum):
     # error handling
     if (max < min or min < 1):
         raise Error("Invalid Range")
     elif (nNum > max - min + 1):
         raise Error("Too many requested numbers")
-    # main logic
-    for x in range(nNum):
-        # no other numbers to pick from
-        if len(seen) >= max-min+1:
-            print("All numbers exhausted")
-            break
 
-        # keep looping if it's a duplicate number
-        num = -1
-        exist = True
-        while (exist == True):
-            num = random.randint(min, max)
-            if num in seen:
-                exist = True
-            else:
-                exist = False
-        # display num then add it to seen list
-        print(num)
-        seen.append(num)
-
-
-def main():
-    min = int(input("Min: "))
-    max = int(input("Max: "))
-    nNum = int(input("nNum: "))
-
-    flag = 1
-    i = 1
-    while (flag == 1):
-        try:
-            RNG(min, max, nNum, i)
-            newFlag = input("Again(Y/N)?")
-            newFlag = newFlag.lower()
-            if newFlag == 'n':
-                flag = 0
-        except Error as e:
-            print(e)
-            break
-        i += 1
-main()
