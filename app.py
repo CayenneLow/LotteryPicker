481f5f05dfd5f3bbbc628f9e98a13f179964e4aa
Khye Ean Low <lowkhyeean@gmail.com>
2019-09-28 20:04:53 +1000

    fixed bug where it was still rolling eventho exhausted

--- app.py
+++ app.py
@@ -18,6 +18,8 @@ def index():
         session['min'] = min
         session['max'] = max
         session['nNums'] = nNums
+        session['rolls'] = 0
+        session['exhausted'] = False;
         try:
             checks(int(min), int(max), int(nNums))
         except Error as e:
@@ -43,14 +45,16 @@ def display():
     if 'seen' not in session:
         session['seen'] = []     
     seenList = session.get('seen', None)
-    exhausted = False
     try:
-        session['seen'] = RNG(min, max, nNums, seenList)
-        print(session['seen'])
+        if (bool(session.get('exhausted', None)) != True):
+            session['seen'] = RNG(min, max, nNums, seenList)
+            session['rolls'] += 1
+            if len(session.get('seen', None)) == max:
+                session['exhausted'] = True
     except Exhausted as e:
-        exhausted = True
+        session['exhausted'] = True
     indexToStartOutputting = len(seenList) - nNums
-    return render_template('display.html', printList=seenList[indexToStartOutputting:len(seenList)], exhausted=exhausted, nNums=nNums)
+    return render_template('display.html', printList=seenList[indexToStartOutputting:len(seenList)], exhausted=bool(session.get('exhausted', None)), nNums=nNums, rolls=session.get('rolls',None))
 
     
 
