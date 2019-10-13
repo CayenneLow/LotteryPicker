12c1d569d073612759bf8e568ad4857bb4116303
Khye Ean Low <lowkhyeean@gmail.com>
2019-10-14 01:00:37 +1100

    added 404 and 500 pages

--- app.py
+++ app.py
@@ -113,5 +113,13 @@ def save():
         return redirect(url_for('index'))
     return send_from_directory(directory=savesDir, filename=fileName + ".csv", as_attachment=True)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 404
+
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
